4d97007255f150e727a49709e8a534889f568873
Debora Sujono <deborausujono@gmail.com>
2016-12-13 19:40:00 -0500

    Added option to use small data

--- pixel_rnn.py
+++ pixel_rnn.py
@@ -288,6 +288,9 @@ def make_minibatch(X_train, y_train, batch_size):
 
     return X_batch, y_batch
 
+if len(sys.argv) < 2:
+    print 'Usage: python baseline.py use_small_data'
+
 print 'Compiling network'
 
 # inputs.shape: (batch size, height, width, channels)
@@ -371,10 +374,16 @@ data = get_CIFAR10_data(mode=1)
 for k, v in data.iteritems():
     print '%s: ' % k, v.shape
 
-y_train = data['y_train']
 X_train = data['X_train']
+y_train = data['y_train']
 small_X = X_train[:2]
 small_y = y_train[:2]
+
+use_small_data = bool(int(sys.argv[1]))
+if use_small_data:
+    X_train = X_train[:100]
+    y_train = y_train[:100]
+
 num_train = X_train.shape[0]
 
 print "Training!"
